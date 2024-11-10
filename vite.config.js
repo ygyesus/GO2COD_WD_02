d71db7ad5e7b36f7d73db051c9e100464fbaf0fe
ygyesus <yosefgyesus.in@gmail.com>
2024-11-10 09:39:19 +0300

    Edit vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+    define: {
+    'process.env.VITE_API_KEY': import.meta.env.VITE_API_KEY,
+  },
 })
